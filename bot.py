0a3f1df0f2651028ccc71b0894df43eca2df32aa
Amanda Krauss <akrauss@i-122624.local>
2017-06-13 15:22:44 -0500

    add basic bot script

---
+++ bot.py
@@ -0,0 +1,27 @@
+import requests
+import nltk
+import os
+import tweepy
+
+# in order to tweet, you'll need app keys
+# you can create environment variables in your own env
+# using autoenv or by using export statements
+consumer_key = os.environ.get('consumer_key')
+consumer_secret = os.environ.get('consumer_secret')
+access_token = os.environ.get('access_token')
+access_token_secret = os.environ.get('access_token_secret')
+
+def make_str():
+	# create whatever sentences here
+
+def make_tweet(str):
+    # get Twitter access
+    try:
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+    except:
+        print("Error: Authentication failed")
+
+    # tweet the string
+    api.update_status(str)
